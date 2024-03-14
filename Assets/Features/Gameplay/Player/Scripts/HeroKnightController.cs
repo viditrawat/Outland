7599947a488f6fda7bedc452abf8f3d16decb97f
CRIMEMASTERGOGO\vidit <vrawat2@my.cetennialcollege.ca>
2024-03-14 09:10:58 -0400

    warrior player created

--- Assets/Features/Gameplay/Player/Scripts/HeroKnightController.cs
+++ Assets/Features/Gameplay/Player/Scripts/HeroKnightController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class HeroKnightController : MonoBehaviour
 {
 
     #region [========== variables ==========]
---
+++ Assets/Features/Gameplay/Player/Scripts/WarriorController.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class WarriorController : MonoBehaviour
+{
+
+    private float XInput;
+
+
+    #region [========== Components ==========]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private LayerMask groundMask;
+    #endregion
+
+    #region [=========== variables ============]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float groundCheckDistance;
+    private bool isGrounded;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Movement();
+        XInput = Input.GetAxisRaw("Horizontal");
+        CollisionChecks();
+
+
+       // FlipController();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
+        animator.SetFloat("yVelocity", rb.velocity.y);
+        animator.SetBool("isGrounded", isGrounded);
+
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundMask);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(XInput * moveSpeed, rb.velocity.y);
+        animator.SetBool("isMoving", rb.velocity.x != 0);
+        spriteRenderer.flipX = rb.velocity.x < 0;
+
+    }
+
+}
