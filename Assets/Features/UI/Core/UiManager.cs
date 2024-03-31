d4d04f58c8b8aac462af69103bc403995fa34578
Rozhin <rdayyani@my.centennialcollege.ca>
2024-03-31 16:48:16 -0400

    Main Menu Added

---
+++ Assets/Features/UI/Core/UiManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    public static UiManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    public void OpenLevel(string _level)
+    {
+        SceneManager.LoadScene(_level);
+    }
+}
+
