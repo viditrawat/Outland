2638733f9bdac5b8331609821d232a8b9efe0a7c
CRIMEMASTERGOGO/Vidit <vrawat2@my.centennialcollege.ca>
2024-04-05 19:47:50 -0400

    background mving

---
+++ Assets/Features/Gameplay/Core/Scripts/ParallaxBackground.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField] private GameObject cam;
+    [SerializeField] private float parallaxEffect;
+
+    private float xPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToMove = cam.transform.position.x * parallaxEffect;
+        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+    }
+}
